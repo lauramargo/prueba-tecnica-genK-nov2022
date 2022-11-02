d580b4e0fa2e32f50cb1c94ef9a08bf5aa64f86e
Laura Martin gonzalez <lauramartingonzalez@MacBook-Pro-de-Laura.local>
2022-11-02 20:45:02 +0100

    text readMe 2a

---
+++ ejercicio2a.js
@@ -0,0 +1,14 @@
+const array1 = [
+    [1, 3, 5],
+    [7, 9, 7],
+    [5, 3, 1],
+];
+
+const array2 = [
+    [2, 4, 6],
+    [8, 10, 8],
+    [6, 4, 2],
+];
+/* array1 valores (xy) y array2 valores (yx) multiplicarlos y meter los valores resultantes en un nuevo array result*/
+const result = [];
+
