8d73516ff3ecc2ce716c6fccb7c3410ffb4dcd58
Laura Martin gonzalez <lauramartingonzalez@MacBook-Pro-de-Laura.local>
2022-11-02 19:19:49 +0100

    create button and change last letter to capital letter

---
+++ ejercicio1b.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const button1 = document.createElement("button");
+button1.innerText = '1 botón';
+button1.id = 'button1';
+button1.className = 'button';
+
+document.body.appendChild(button1);
+
+const buttonText = document.getElementById('button1');
+
+buttonText.onmouseenter = () => {
+    buttonText.innerHTML = '1 botóN';
+}
+
+buttonText.onmouseleave = () => {
+    buttonText.innerHTML = '1 botón';
+}
